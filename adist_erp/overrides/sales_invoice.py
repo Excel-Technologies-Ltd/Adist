1ec47b89a0c4e9f1dfa5ae09e7a4b0e14b279943
sohan653 <sohanurl653@gmail.com>
2025-05-10 02:45:49 +0000

    fix mobile no

--- adist_erp/overrides/sales_invoice.py
+++ adist_erp/overrides/sales_invoice.py
@@ -6,7 +6,7 @@ from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
 
 class NewSalesInvoice(SalesInvoice):
     def on_update_after_submit(self):
-        get_customer_mobile_number=frappe.db.get_value('Customer', self.customer, 'mobile_number')
+        get_customer_mobile_number=frappe.db.get_value('Customer', self.customer, 'mobile_no')
         if get_customer_mobile_number and self.delivery_status == "Delivery Completed":
             sms_message = f"Dear {self.customer_name}, your sales invoice {self.name} has been delivered"
             send_sms([get_customer_mobile_number], sms_message,success_msg=False)
