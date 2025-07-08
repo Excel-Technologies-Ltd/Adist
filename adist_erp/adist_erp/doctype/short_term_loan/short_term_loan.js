036447e9f1302d5955199553e3939a00e31bb3d4
sohan653 <sohanurl653@gmail.com>
2025-07-08 07:51:57 +0000

    add short term loan doctype

---
+++ adist_erp/adist_erp/doctype/short_term_loan/short_term_loan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, sohan.dev@excelbd.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Short Term Loan', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ adist_erp/adist_erp/doctype/short_term_loan/short_term_loan.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, sohan.dev@excelbd.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class ShortTermLoan(Document):
+	pass
