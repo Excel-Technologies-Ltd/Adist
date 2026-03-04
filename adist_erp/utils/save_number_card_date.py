1321dfe47e828f1d2b16b513056fc1112141a46f
excel-azmin <excel.azmin@gmail.com>
2026-03-04 05:21:14 +0000

    feat fix card value update issue

--- adist_erp/utils/save_number_card_date.py
+++ adist_erp/utils/save_number_card_date.py
@@ -1,14 +1,22 @@
 
 import frappe
+import json
 
 
 def save_number_card_date():
 	"""Save A/C Receivables and A/C Payables Number Cards daily at 12:05 AM."""
 	cards = ["A/C Receivables", "A/C Payables"]
+	today = frappe.utils.formatdate(frappe.utils.now_datetime().date(), "yyyy-mm-dd")
 
 	for card_name in cards:
 		try:
 			doc = frappe.get_doc("Number Card", card_name)
+			if doc.filters_json:
+				filters = json.loads(doc.filters_json)
+				if isinstance(filters, dict) and "report_date" in filters:
+					filters["report_date"] = today
+					doc.filters_json = json.dumps(filters)
+
 			doc.save(ignore_permissions=True)
 			frappe.db.commit()
 			frappe.logger().info(f"Number Card '{card_name}' saved successfully.")
